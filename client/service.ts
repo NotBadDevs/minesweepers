072506d58b327ae27a3d8bf06c2b0576b8943a9e
Maksym Nesov <mnesov@cogniance.com>
2019-03-31 23:16:03 +0200

    node env logic correction

--- client/service.ts
+++ client/service.ts
@@ -3,8 +3,8 @@ import io from 'socket.io-client'
 import { SOCKET_IO_PORT } from '../shared/const'
 
 export const service = io(
-  process.env.NODE_ENV === 'production'
-    ? `http://cubie.dns-cloud.net:${SOCKET_IO_PORT}/`
-    : `http://localhost:${SOCKET_IO_PORT}`,
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:${SOCKET_IO_PORT}`
+    : `http://cubie.dns-cloud.net:${SOCKET_IO_PORT}/`,
   { transports: ['websocket'] }
 )
