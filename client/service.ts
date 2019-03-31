d199908d90313c80e62b82aa0d962451b119456c
Maksym Nesov <mnesov@cogniance.com>
2019-04-01 00:37:26 +0200

    add dockerfile to client

--- client/service.ts
+++ client/service.ts
@@ -2,6 +2,8 @@ import io from 'socket.io-client'
 
 import { SOCKET_IO_PORT } from '../shared/const'
 
+console.log('env', process.env.NODE_ENV, process.env)
+
 export const service = io(
   process.env.NODE_ENV === 'development'
     ? `http://localhost:${SOCKET_IO_PORT}`
